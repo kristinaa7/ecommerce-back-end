87ce5af1f3961cc62486c2e9005d81019c62cac9
Kristina Alabastro <kristinaa7@live.com>
2023-02-25 00:42:04 -0800

    added new files

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,15 +6,28 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll().then((categoryData) => {
+    res.json(categoryData);
+});
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id).then((categoryData) => {
+    res.json(categoryData);
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then((newCategory) => {
+    res.json(newCategory);
+  })
+  .catch((err) => {
+  res.json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # E-commerce Back End Starter Code
+
+## Description
+The following application is a fully function e-commerce site. My role as a developer was to successfully build the back end for this business site using the already created front end code.
+
+## Usage
+
+## License
+
+## Technology Used 
+* Sequelize
+* MySQL
+* Node js
+* Insomnia
+
+### Walkthrough Link
