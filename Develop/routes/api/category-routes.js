4a29e9fa9d3f2f5476e7d1cbbaf2f2b57b2b0e85
Kristina Alabastro <kristinaa7@live.com>
2023-02-25 00:50:25 -0800

    added delete and find one

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -14,7 +14,16 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  Category.findByPk(req.params.id).then((categoryData) => {
+  // Category.findByPk(req.params.id).then((categoryData) => {
+  //   res.json(categoryData);
+  // })
+  Category.findOne(
+  {
+    where: {
+      id: req.params.id
+    },
+  })
+  .then((categoryData) => {
     res.json(categoryData);
   })
 });
@@ -50,6 +59,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((deletedCategory) => {
+    res.json(deletedCategory);
+  })
+  .catch((err) => res.json(err));
 });
 
 module.exports = router;
