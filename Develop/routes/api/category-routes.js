015c02526f013f2f66eaf583fc6002506f06d380
Kristina Alabastro <kristinaa7@live.com>
2023-02-27 01:35:19 -0600

    added products to router get id

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -20,11 +20,15 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  Category.findOne(
+  Category.findByPk(
   {
     where: {
       id: req.params.id
     },
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
   })
   .then((categoryData) => {
     res.json(categoryData);
