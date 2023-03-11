fe4da663bb244aa9dd04482154df2d7ff359242b
Kristina Alabastro <kristinaa7@live.com>
2023-03-11 08:18:36 -0800

    delete node_modules and reinstalled dependencies

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -29,11 +29,7 @@ router.get('/:id', async (req, res) => {
       attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
     }
   });
- if (!categoryData) {
-  res.status(400).json(err);
-  return;
- }
- res.status(200).json(categoryData);
+  res.status(200).json(categoryData);
 } catch (err) {
   res.status(500).json(err);
 }
@@ -42,7 +38,11 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
     try {
-  const categoryData = await Category.create(req.body);
+  const categoryData = await Category.create({
+    category_name: req.body.category_name
+  },
+  {where: {id: req.params.id,}
+});
   res.status(200).json(categoryData);
 } catch (err) {
   res.status(400).json(err);
@@ -54,18 +54,13 @@ router.put('/:id', async (req, res) => {
   try {
  const categoryData = await Category.update(
     {
-      category_name: req.body.category_name,
+      category_name: req.body.category_name
     },
     {
       where: {
         id: req.params.id,
       },
     });
-
-    if (!categoryData[0]) {
-      res.status(404).json({ message: 'No category with this id!' });
-      return;
-    }
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
