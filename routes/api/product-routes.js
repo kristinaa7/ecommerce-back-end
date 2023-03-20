0fc163b7fdbae5cdda6175d266c57d6de33a03bb
Kristina Alabastro <kristinaa7@live.com>
2023-03-20 08:08:40 -0700

    debugged code

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -50,20 +50,25 @@ router.get('/:id',  (req, res) => {
 
 // create new product
 router.post('/', async (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
- try {
-  const userData = await Product.create(req.body)
-  res.status(200).json(categoryData);
-} catch (err) {
-  res.status(400).json(err);
-}
+ Product.create(req.body)
+    .then((product) => {
+      if (req.body.tagIds && req.body.tagIds.length) {
+        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+          return {
+            product_id: product.id,
+            tag_id,
+          };
+        });
+        return ProductTag.bulkCreate(productTagIdArr);
+      }
+      // if no product tags, just respond
+      res.status(200).json(product);
+    })
+    .then((productTagIds) => res.status(200).json(productTagIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 // update product
